a94ea43d71eb16c7097bddf861a254289161779b
mac duc anh <gumayusi9602@outlook.com>
2023-12-25 12:41:26 +0700

    test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts{
+    
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //abc
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+    
+}
