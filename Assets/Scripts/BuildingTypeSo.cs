f1c2651c8b4152c89ec29549cb1607298da0cc33
mac duc anh <gumayusi9602@outlook.com>
2023-12-26 10:52:59 +0700

    setup folder

---
+++ Assets/Scripts/BuildingTypeSo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+    [CreateAssetMenu(menuName = "SO/BuildingTypeSO")]
+        public class BuildingTypeSo : ScriptableObject
+        {
+            public string nameBuilding;
+            public Transform prefabs;
+        }
+        
+    
---
+++ Assets/Scripts/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts{
+    
+    public static class Extension
+    {      
+       public static Vector2 getMousePosition()
+       {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       }
+
+        public static Vector2 getRandomPos()
+        {
+            return new Vector2(Random.Range(-2, 2) , Random.Range(-2,2));
+        }
+    }
+    
+}
